d67d22b191b127e8edc458da2acdf5bcb28eed79
Sharon <sharonkunnel2003@gmail.com>
2024-03-21 22:48:44 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", null, [
+  React.createElement("div", null, [
+    React.createElement("h1", null, "Hi This is h1 tag"),
+    React.createElement("h2", null, "Hi This is h2 tag"),
+  ]),
+  React.createElement("div", null, [
+    React.createElement("h1", null, "Hi This is h1 tag"),
+    React.createElement("h2", null, "Hi This is h2 tag"),
+  ]),
+]);
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
