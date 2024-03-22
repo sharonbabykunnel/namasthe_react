3010f09b0fab16c4a41a5adcdf97cd61451c85a5
Sharon <sharonkunnel2003@gmail.com>
2024-03-22 22:41:55 +0530

    second-day

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const parent = React.createElement("div", null, [
   React.createElement("div", null, [
     React.createElement("h1", null, "Hi This is h1 tag"),
