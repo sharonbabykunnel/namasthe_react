c85ccd5f120dd6a4f64b497009a7f02d04522b34
Sharon <sharonkunnel2003@gmail.com>
2024-03-23 22:53:56 +0530

    23-03

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-const parent = React.createElement("div", null, [
-  React.createElement("div", null, [
-    React.createElement("h1", null, "Hi This is h1 tag"),
-    React.createElement("h2", null, "Hi This is h2 tag"),
-  ]),
-  React.createElement("div", null, [
-    React.createElement("h1", null, "Hi This is h1 tag"),
-    React.createElement("h2", null, "Hi This is h2 tag"),
-  ]),
-]);
-console.log(parent)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+const Jsxheading = () => (<h1 id="heading" >namasthe React</h1>);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Jsxheading/>);
